30c5fa8310093c468fe27bea90ccbc3a711be7ee
tanakachamisa <tanakachamisa56@gmail.com>
2020-01-05 17:54:58 +0200

    Updates

---
+++ precache-manifest.d70257e56e41393bf54d889e791a4491.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecc74dba0d33bad7b2f8e5ac2f0215f9",
+    "url": "/react-monster-app/index.html"
+  },
+  {
+    "revision": "bf2e1ddf7e4d77913ed4",
+    "url": "/react-monster-app/static/css/main.49129d3e.chunk.css"
+  },
+  {
+    "revision": "49c5f1b646bcf07ccf8b",
+    "url": "/react-monster-app/static/js/2.64a07eca.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-monster-app/static/js/2.64a07eca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bf2e1ddf7e4d77913ed4",
+    "url": "/react-monster-app/static/js/main.b9fec8bf.chunk.js"
+  },
+  {
+    "revision": "38597f12516003c0f5d0",
+    "url": "/react-monster-app/static/js/runtime-main.cd168ad6.js"
+  }
+]);
